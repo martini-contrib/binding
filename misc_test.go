18328fdbf5f7d42dac7a6633967f9c12eda1f2d1
Matthew Holt <Matthew.Holt+git@gmail.com>
2014-04-27 17:45:19 -0600

    The tests are done; all assertions added for known cases

--- misc_test.go
+++ misc_test.go
@@ -10,24 +10,46 @@ import (
 	"github.com/go-martini/martini"
 )
 
-// You might notice that these tests do not actually assert anything.
-// They just output the result of the binding to the console.
-// With these tests, we get significantly better coverage, but
-// when somebody has the time, adding assertions would be great.
+// When binding from Form data, testing the type of data to bind
+// and converting a string into that type is tedious, so these tests
+// cover all those cases.
 func TestSetWithProperType(t *testing.T) {
-	httpRecorder := httptest.NewRecorder()
-	m := martini.Classic()
-
-	testCases := map[string]string{
+	testInputs := map[string]string{
 		"successful": `integer=-1&integer8=-8&integer16=-16&integer32=-32&integer64=-64&uinteger=1&uinteger8=8&uinteger16=16&uinteger32=32&uinteger64=64&boolean_1=true&fl32_1=32.3232&fl64_1=-64.6464646464&str=string`,
 		"errorful":   `integer=&integer8=asdf&integer16=--&integer32=&integer64=dsf&uinteger=&uinteger8=asdf&uinteger16=+&uinteger32= 32 &uinteger64=+%20+&boolean_1=&boolean_2=asdf&fl32_1=asdf&fl32_2=&fl64_1=&fl64_2=asdfstr`,
 	}
 
-	m.Post(testRoute, Form(Everything{}), func(actual Everything, errs Errors) {
-		fmt.Printf("%+v\n", actual)
-	})
+	expectedOutputs := map[string]Everything{
+		"successful": Everything{
+			Integer:    -1,
+			Integer8:   -8,
+			Integer16:  -16,
+			Integer32:  -32,
+			Integer64:  -64,
+			Uinteger:   1,
+			Uinteger8:  8,
+			Uinteger16: 16,
+			Uinteger32: 32,
+			Uinteger64: 64,
+			Boolean_1:  true,
+			Fl32_1:     32.3232,
+			Fl64_1:     -64.6464646464,
+			Str:        "string",
+		},
+		"errorful": Everything{},
+	}
 
-	for _, testCase := range testCases {
+	for key, testCase := range testInputs {
+		httpRecorder := httptest.NewRecorder()
+		m := martini.Classic()
+
+		m.Post(testRoute, Form(Everything{}), func(actual Everything, errs Errors) {
+			actualStr := fmt.Sprintf("%+v", actual)
+			expectedStr := fmt.Sprintf("%+v", expectedOutputs[key])
+			if actualStr != expectedStr {
+				t.Errorf("For '%s' expected\n%s but got\n%s", key, expectedStr, actualStr)
+			}
+		})
 		req, err := http.NewRequest("POST", testRoute, strings.NewReader(testCase))
 		if err != nil {
 			panic(err)
